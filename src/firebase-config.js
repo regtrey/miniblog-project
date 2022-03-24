c8145d495b9cd5ff793a58049262d36300d9b684
regtrey <manigosreggie@gmail.com>
2022-03-24 13:27:56 +0800

    firebase changes

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from '@firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: 'AIzaSyBl0ltkq49feo8N2Bi9hXYCWzIzvLujywU',
+  authDomain: 'miniblog-project-c391b.firebaseapp.com',
+  projectId: 'miniblog-project-c391b',
+  storageBucket: 'miniblog-project-c391b.appspot.com',
+  messagingSenderId: '449016168821',
+  appId: '1:449016168821:web:fa8cfc01d63af5f2c3d33c',
+  measurementId: 'G-1RJ4PEY929',
 };
 
 const app = initializeApp(firebaseConfig);
